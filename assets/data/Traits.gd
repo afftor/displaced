78913ce06b5af5e268628ca03e517314f4743e28
afftor <afftor2@gmail.com>
2019-02-04 03:08:44 +0300

    subcommit

--- assets/data/Traits.gd
+++ assets/data/Traits.gd
@@ -1,7 +1,8 @@
 extends Node
 
 var traitlist = {
-	damagebeast = {name = tr('DAMAGEBEAST'), descript = tr("DAMAGEBEASTDESCRIPT"), effects = [{trigger = 'anyhit', triggereffect = 'damagebeast'}]}
+	damagebeast = {name = tr('DAMAGEBEAST'), descript = tr("DAMAGEBEASTDESCRIPT"), effects = [{trigger = 'anyhit', triggereffect = 'damagebeast'}]},
+	protectbeast = {name = tr('PROTECTBEAST'), descript = tr("PROTECTBEASTDESCRIPT"), effects = [{trigger = 'damagetaken', triggereffect = 'protectbeast'}]},
 	#bonebow = {descript = tr("BONEBOWDESCRIPT"), code = 'bonebow', textcolor = 'yellow', trigger = 'skillhit', triggereffect = 'boneboweffect'},
 	
 	
