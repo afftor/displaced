b834de2c849c0dc9e6fd04810a398acce8ed86c3
ykoc <rak3new@gmail.com>
2022-06-28 19:02:46 +0300

    res_for_tests

--- files/scenes/combat/charpanel.gd
+++ files/scenes/combat/charpanel.gd
@@ -12,3 +12,8 @@ func build_for_hero(hero):
 
 func build_for_fighter(fighter):
 	ch_id = fighter
+	$Label.text = ""
+	var person = state.heroes[ch_id]
+	var data = person.get_resists()
+	for res in data:
+		$Label.text += "%s: %.1f\n" % [res, data[res]]
