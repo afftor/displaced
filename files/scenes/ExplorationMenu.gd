9d063a195e8d68f7e852b30494ec6737f9a1a642
ykoc <rak3new@gmail.com>
2023-03-10 03:50:37 +0300

    more_fix

--- files/scenes/ExplorationMenu.gd
+++ files/scenes/ExplorationMenu.gd
@@ -126,8 +126,8 @@ func select_area(area_code):
 		$ExplorationSelect/Panel/advance.visible = false
 		$ExplorationSelect/Panel/abandon.visible = false
 		scalecheck.set_state(false)
-		scalecheck.visible = !state.areaprogress[area].completed
-		$ExplorationSelect/Panel/Label2.visible = !state.areaprogress[area].completed
+		scalecheck.visible = state.areaprogress[area].completed
+		$ExplorationSelect/Panel/Label2.visible = state.areaprogress[area].completed
 	elif state.activearea != area:
 		$ExplorationSelect/Panel/start.visible = true
 		$ExplorationSelect/Panel/start.disabled = true
